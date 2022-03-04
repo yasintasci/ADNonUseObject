90553b4718c4c34ebd9275afe6ee4aee4b42f519
Yasin Tasci <100833671+yasintasci@users.noreply.github.com>
2022-03-04 15:35:53 +0300

    Add files via upload

---
+++ BulkGroupCreateFromCSV.ps1
@@ -0,0 +1,13 @@
+﻿$GroupLocation ="OU=Groups,OU=***-test,DC=systeam3,DC=work"
+$Groups = Import-Csv -Path C:\temp\groups.csv
+foreach ( $Group in $Groups){
+if ( -not (Get-ADGroup -Filter "Name -eq '$($Group.Group)'" -ErrorAction SilentlyContinue))
+{
+New-ADGroup -Name $Group.Group -SamAccountName $Group.SamAccountName -Description "'$($Group.Group)' Takımı için oluşturulmuş Security grubudur" -Path $GroupLocation -GroupScope Universal -GroupCategory Security
+
+}
+else
+{"Group '$($Group.Group)' already exist" }
+
+
+} 
